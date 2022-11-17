dec418737d5031bcde06eef394ac06cb1584eb51
emonShimoul <emonshimoul@gmail.com>
2022-11-17 06:57:23 +0600

    categories component

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div>
+            <h2>Category</h2>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Header from '../../Shared/Header/Header';
 import Banner from '../Banner/Banner';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Banner></Banner>
+            <Categories></Categories>
             <Footer></Footer>
         </div>
     );
