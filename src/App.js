3d48d1461089995c1201acc84bfad42dce4360fd
emonShimoul <emonshimoul@gmail.com>
2022-11-18 00:17:44 +0600

    slight changwes'

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header></Header>
         <Routes>
           <Route element={<Login />} path="/login"></Route>
           <Route element={<Register />} path="/register"></Route>
           <Route element={<Home />} path="/"></Route>
         </Routes>
+        <Footer></Footer>
       </BrowserRouter>
     </div>
   );
