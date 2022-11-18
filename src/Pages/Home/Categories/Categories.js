705c54503239e529a51130c10296c3b8ada8430c
emonShimoul <emonshimoul@gmail.com>
2022-11-18 23:54:00 +0600

    added grid layout for categories

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
     return (
         <div className='container mx-auto'>
-            <h2>Categories</h2>
+            <div class="2xl:ml-28 grid lg:grid-rows-3 lg:grid-flow-col gap-8 my-20">
+                <div class="lg:row-span-3">
+                    <Link>
+                        <img className='lg:float-right' src="https://htmldemo.net/pustok/pustok/image/others/cat-gal-large.png" alt="" />
+                    </Link>
+                </div>
+                <div class="lg:col-span-3">
+                    <Link><img className='lg:float-left lg:mr-8' src="https://htmldemo.net/pustok/pustok/image/others/cat-gal-mid.png" alt="" /></Link>
+                    <Link><img className='' src="https://htmldemo.net/pustok/pustok/image/others/cat-gal-small.png" alt="" /></Link>
+                </div>
+                <div class="lg:col-span-3">
+                    <Link><img className='lg:float-left lg:mr-8' src="https://htmldemo.net/pustok/pustok/image/others/cat-gal-small-2.jpg" alt="" /></Link>
+                    <Link><img className='' src="https://htmldemo.net/pustok/pustok/image/others/cat-gal-mid-2.png" alt="" /></Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className='mt-20'>
             <h2>Footer</h2>
         </div>
     );
