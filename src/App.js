9ca1a5f23df5e7928f4dcb8f0f1d55b0ec92a0f5
emonShimoul <emonshimoul@gmail.com>
2022-11-20 21:29:28 +0600

    add product details page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import AuthProvider from './contexts/AuthProvider/AuthProvider';
 import AddBooks from './Pages/Dashboard/AddBooks/AddBooks';
 import Home from './Pages/Home/Home/Home';
+import ProductDetails from './Pages/Home/ProductDetails/ProductDetails';
 import Login from './Pages/Login/Login/Login';
 import ProtectedRoute from './Pages/Login/ProtectedRoute/ProtectedRoute';
 import Register from './Pages/Login/Register/Register';
@@ -18,6 +19,7 @@ function App() {
           <Routes>
             <Route element={<Login />} path="/login"></Route>
             <Route element={<Register />} path="/register"></Route>
+            <Route element={<ProductDetails />} path="/productDetails"></Route>
             <Route element={<ProtectedRoute><AddBooks /></ProtectedRoute>} path="/addbooks"></Route>
             <Route element={<Home />} path="/"></Route>
           </Routes>
