7382d9fee76d2d32ade029dff8f4a7d685903a1a
emonShimoul <emonshimoul@gmail.com>
2022-12-18 13:47:52 +0600

    added image field for books

--- src/Pages/Dashboard/AddBooks/AddBooks.js
+++ src/Pages/Dashboard/AddBooks/AddBooks.js
@@ -132,18 +132,26 @@ const AddBooks = () => {
                         </div>
 
                         <div className="mb-2">
-                            <label
+                            {/* <label
                                 htmlFor="image"
                                 className="block text-sm font-semibold text-gray-800"
                             >
                                 Image
-                            </label>
-                            <input
+                            </label> */}
+                            {/* <input
                                 onBlur={handleOnChange}
                                 name="image"
                                 type="text"
                                 className="block w-full px-4 py-2 mt-2 text-green-700 bg-white border rounded-md focus:border-green-400 focus:ring-green-300 focus:outline-none focus:ring focus:ring-opacity-40" required
-                            />
+                            /> */}
+
+                            <input type="file" id="file-upload" className="hidden" />
+                            <label htmlFor="file-upload" className="z-20 mx-auto mt-4 py-4 rounded flex flex-col-reverse items-center justify-center w-1/2 h-full cursor-pointer border border-dotted border-gray-600">
+                                <p className="z-10 text-xs font-bold text-center">Drag & Drop your image here</p>
+                                <svg className="z-10 w-8 h-8 text-red-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"></path>
+                                </svg>
+                            </label>
                         </div>
 
                         <div className="mt-6">
