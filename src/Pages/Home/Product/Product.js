1994e9b73bcdcf1e1e522ef887ba21cef0fab0af
emonShimoul <emonshimoul@gmail.com>
2022-12-20 23:45:39 +0600

    color of add to cart button

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -20,7 +20,7 @@ const Product = ({ bookData }) => {
                 </div>
                 <div className="flex items-center justify-between">
                     <span className="text-2xl font-bold text-gray-900 dark:text-white">{price} tk</span>
-                    <Link href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</Link>
+                    <Link href="#" className="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</Link>
                 </div>
             </div>
         </div>
