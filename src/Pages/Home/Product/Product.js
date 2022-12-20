47f2c347987ae38acca3a54e2b227d7491d571f9
emonShimoul <emonshimoul@gmail.com>
2022-12-20 23:47:55 +0600

    button hover color changes

--- src/Pages/Home/Product/Product.js
+++ src/Pages/Home/Product/Product.js
@@ -20,7 +20,7 @@ const Product = ({ bookData }) => {
                 </div>
                 <div className="flex items-center justify-between">
                     <span className="text-xl font-bold text-gray-900 dark:text-white">{price} TK</span>
-                    <Link href="#" className="text-white bg-green-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Cart</Link>
+                    <Link href="#" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Cart</Link>
                 </div>
             </div>
         </div>
